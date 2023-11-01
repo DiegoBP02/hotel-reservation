d0a173db180debf29a6e2ced9ed13dab86bcc492
DiegoBP02 <diegobpcelular@gmail.com>
2023-11-01 16:21:55 -0300

    initial commit

---
+++ backend/authentication-service/src/test/java/com/bpdev/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpdev.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/eureka-server/src/test/java/com/bpdev/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpdev.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
